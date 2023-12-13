e575c558d3dd0f558cfc3cb007aa8fb3fef5d214
Nicolas de Campos Fischer <79231180+NicolasFischer2002@users.noreply.github.com>
2023-12-12 22:02:39 -0300

    Add files via upload

---
+++ Task.WhenAll/Task_WhenAll/Task_WhenAll/Exemplos.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_WhenAll
+{
+    internal class Exemplos
+    {
+        public static async Task CriaDiretoriosEmLoop(string caminhoDiretorio, string nomeDiretorio)
+        {
+            string novaPasta = $"{caminhoDiretorio}\\{nomeDiretorio}";
+            string novaPastaDinamica = novaPasta;
+
+            for (int i = 1; i < 11; i++)
+            {
+                if (!Directory.Exists(novaPastaDinamica))
+                {
+                    Directory.CreateDirectory(novaPastaDinamica);
+                    
+                    Console.WriteLine("Diretório criado com sucesso!");
+                    await Task.Delay(1000);
+                }
+                novaPastaDinamica = $"{novaPasta}({i})";
+            }
+        }
+
+        public static async Task CriaArquivosEmLoop(string caminhoArquivo, string nomeArquivo, string extensao)
+        {
+            string novoArquivo = $"{caminhoArquivo}\\{nomeArquivo}{extensao}";
+            string[] conteudos = ArrayConteudos();
+            string conteudo;
+            string novoArquivoDinamico = novoArquivo;
+            Random random = new();
+            int numeroAleatorio;
+
+            for (int i = 1; i < 11; i++)
+            {
+                numeroAleatorio = random.Next(0, 10);
+                conteudo = conteudos[numeroAleatorio];
+
+                if (!File.Exists(novoArquivoDinamico))
+                {
+                    File.WriteAllText(novoArquivoDinamico, conteudo);
+                    Console.WriteLine("Arquivo Criado com sucesso!");
+                    await Task.Delay(2000);
+
+                    novoArquivoDinamico = $"{caminhoArquivo}\\{nomeArquivo}({i}){extensao}";
+                }
+            }
+        }
+
+        public static string[] ArrayConteudos()
+        {
+            string[] conteudos = 
+                    ["Se Deus não existe, tudo é permitido. (Dostoiévski)",
+                    "Nada é permanente, exceto a mudança. (Heráclito de Éfeso)", 
+                    "Só sei que nada sei. (Sócrates)",
+                    "Uma vida sem reflexão não vale a pena ser vivida. (Sócrates)",
+                    "Creio para compreender e compreendo para crer melhor. (Santo Agostinho)",
+                    "O desordenado amor por si mesmo é a causa de todos os pecados. (São Tomás de Aquino)",
+                    "Penso, logo existo. (Descartes)",
+                    "O homem é o lobo do homem. (Hobbes)",
+                    "Onde não há lei, não há liberdade. (Locke)",
+                    "Compara-se muitas vezes a crueldade do homem à das feras, mas isso é injuriar estas últimas. (Dostoiévski)"];
+
+            return conteudos;
+        }
+
+        public static string GetDesktopPath()
+        {
+            // Obter o caminho do diretório da área de trabalho
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            return desktopPath;
+        }
+    }
+}
---
+++ Task.WhenAll/Task_WhenAll/Task_WhenAll/Program.cs
@@ -0,0 +1,26 @@
+﻿using Task_WhenAll;
+
+try
+{
+    string caminhoDesktop = Exemplos.GetDesktopPath();
+	string nomeDiretorio = "PastasEmLoop";
+    string nomeArquivo = "ArquivosEmLoop";
+
+    List<Task> tasks =
+        [
+            Exemplos.CriaDiretoriosEmLoop(caminhoDesktop, nomeDiretorio),
+            Exemplos.CriaArquivosEmLoop(caminhoDesktop, nomeArquivo, ".txt")
+        ];
+
+	Task task = Task.WhenAll(tasks);
+	task.Wait();
+
+    if (task.IsCompletedSuccessfully)
+        Console.WriteLine("\n\nPrograma finalizado sem erros!");
+    else
+        Console.WriteLine("\n\nPrograma encerrado com erros!");
+}
+catch (Exception e)
+{
+    Console.WriteLine("\n\nAlgum erro inesperado ocorreu: " + e.Message);
+}
